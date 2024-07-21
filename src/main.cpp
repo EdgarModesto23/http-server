8c45eacb66205d13f6907480e875b7c1cf09f573
edgar modesto <edgarmodesto23@gmail.com>
2024-07-21 00:34:50 -0700

    fixed echo route

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 using namespace http;
 
 void callback(Response &res, Request &req) {
-  res.setBody(req.getHeader("User-Agent"));
+  res.setBody(req.getUrlParam("str"));
   res.setContentType("text/plain");
   res.setStatus("200 OK");
+
+  cout << "Header: " << req.getHeader("User-Agent") << endl;
 }
 
 void emptyPath(Response &res, Request &req) {
@@ -14,9 +16,16 @@ void emptyPath(Response &res, Request &req) {
   res.setStatus("200 OK");
 }
 
+void readHeader(Response &res, Request &req) {
+  res.setBody(req.getHeader("User-Agent"));
+  res.setContentType("text/plain");
+  res.setStatus("200 OK");
+}
+
 int main(int argc, char **argv) {
   Server server = Server(4221);
   server.registerRoute("GET /", emptyPath);
-  server.registerRoute("GET /user-agent", callback);
+  server.registerRoute("GET /echo/{str}", callback);
+  server.registerRoute("GET /user-agent", readHeader);
   server.listenAndServe();
 }
